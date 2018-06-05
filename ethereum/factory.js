fc890520435314f5af8a3da3f718628edc2abcdc
billtlee <wlee3@hotmail.com>
2018-06-05 18:24:43 +0800

    hard code contract address to test heroku

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -5,7 +5,7 @@ import MyDataFactory from './build/MyDataFactory.json';
 console.log('Getting address from: ', process.env.LOCATION);
 const instance = new web3.eth.Contract (
   JSON.parse(MyDataFactory.interface),
-  process.env.CONTRACTADDRESS
+  '0x600b3ABc0d043a7621E9b158029C9Ea64e94a477'
 );
 
 export default instance;
