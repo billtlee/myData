a2659270e3cae0950d21aa686436061c3f371baf
billtlee <wlee3@hotmail.com>
2018-06-05 17:10:04 +0800

    update factory.js and web3.js to support environment vars for heroku

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,11 @@
-const deployedContract = require('../config/contractAddress.json');
+require('../config/config');
 import web3 from "./web3";
 import MyDataFactory from './build/MyDataFactory.json';
 
-console.log('Getting address from: ', deployedContract.location);
+console.log('Getting address from: ', process.env.LOCATION);
 const instance = new web3.eth.Contract (
   JSON.parse(MyDataFactory.interface),
-  deployedContract.contractAddress
+  process.env.CONTRACTADDRESS
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,5 @@
+require('../config/config');
 import Web3 from 'web3';
-const configData = require('../config/config.json');
 
 let web3;
 
@@ -9,7 +9,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 } else {
   // we are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    configData.rinkby.PROVIDER_URI
+    process.env.PROVIDER_URI
   );
   web3 = new Web3(provider);
 }
