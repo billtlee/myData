0f5bb8561e784ad9a7f4f195ac47d58b53f70297
billtlee <wlee3@hotmail.com>
2018-06-07 17:49:04 +0800

    update new rinkby contract address in factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -5,7 +5,7 @@ import MyDataFactory from './build/MyDataFactory.json';
 console.log('Getting address from: ', process.env.LOCATION);
 const instance = new web3.eth.Contract (
   JSON.parse(MyDataFactory.interface),
-  '0xB4c7010911F07594e6D031266EAeaAE96BA31C4b'
+  '0xfEF23fc7a17D51F55b0E38266a6FF6e8df601873'
 );
 
 export default instance;
